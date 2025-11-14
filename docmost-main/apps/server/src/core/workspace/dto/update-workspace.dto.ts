2c9b9ac503fc957678167ebd447ec633a284f9ba
Isaac Min <isaac.kj.min@gmail.com>
2025-11-14 00:49:23 -0800

    initial

---
+++ docmost-main/apps/server/src/core/workspace/dto/update-workspace.dto.ts
@@ -0,0 +1,25 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateWorkspaceDto } from './create-workspace.dto';
+import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator';
+
+export class UpdateWorkspaceDto extends PartialType(CreateWorkspaceDto) {
+  @IsOptional()
+  @IsString()
+  logo: string;
+
+  @IsOptional()
+  @IsArray()
+  emailDomains: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  enforceSso: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  enforceMfa: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  restrictApiToAdmins: boolean;
+}
